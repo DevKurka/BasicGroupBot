97f32f95b052e72d971bcec41ccfa2c510094206
DevKurka <kampustelnik@outlook.com>
2018-11-01 21:37:26 +0100

    Small fix

--- index.js
+++ index.js
@@ -7,17 +7,19 @@ function scan(methods) {
   methods.fetchNewWall(config.group, cache.cache.wall_posts_scanned[config.group] || 0, 100).then(function(data) {
     if (data[0] != undefined) { cache.cache.wall_posts_scanned[config.group] = data[0].id; cache.save(); }
     for (let i in data) {
-      let content = data[i].body.toUpperCase();
-      let level = 0;
-      for (let word of config.banned_words) {
-        if (content.indexOf(word.toUpperCase()) != -1) {
-          level++;
+      if (data[i] != undefined && data[i].body != undefined && data[i].poster != undefined)
+        let content = data[i].body.toUpperCase();
+        let level = 0;
+        for (let word of config.banned_words) {
+          if (content.indexOf(word.toUpperCase()) != -1) {
+            level++;
+          }
         }
-      }
-      if (level >= config.target_level && data[i].poster.role.rank < config.op_ranks) {
-        console.log(data[i].poster.user.username + " has been banned.");
-        methods.deleteUserPosts(config.group, data[i].poster.user.userId);
-        methods.setRank(config.group, data[i].poster.user.userId, config.ban_role_id);
+        if (level >= config.target_level && data[i].poster.role.rank < config.op_ranks) {
+          console.log(data[i].poster.user.username + " has been banned.");
+          await methods.deleteUserPosts(config.group, data[i].poster.user.userId);
+          await methods.setRank(config.group, data[i].poster.user.userId, config.ban_role_id);
+        }
       }
     }
   });
