882758a50d20efb9564c25fe32ec203e790765ca
DevKurka <kampustelnik@outlook.com>
2019-02-04 19:06:29 +0100

    Update loginToken.js

--- roblox/includes/loginToken.js
+++ roblox/includes/loginToken.js
@@ -1,7 +1,7 @@
 var request = require('request-promise');
 var http = require('./http.js');
 
-module.exports = async function(type, token) {
+module.exports = async function(token) {
   var jar = request.jar();
 
   jar.session = token;
