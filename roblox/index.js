3d04c0e01006cf9d0b2f0f4e59cce06fc93ae53b
DevKurka <kampustelnik@outlook.com>
2019-02-04 19:05:21 +0100

    Funcaptcha login update

--- roblox/index.js
+++ roblox/index.js
@@ -1,9 +1,32 @@
 var fs = require("fs");
 var path = require("path");
-var login = require("./includes/login.js");
 
+/*
+  OUTDATED DUE TO THE FUNCAPTCHA
+  
+var login = require("./includes/login.js");
 module.exports = async function(type, username, password) {
   return await login(type, username, password).then(function(jar) {
+    var methods = {};
+
+    var modulesHolder = fs.readdirSync("./roblox/modules").filter(function(file) {
+      return fs.statSync(path.join("./roblox/modules", file)).isDirectory();
+    });
+    for (let i = 0; i < modulesHolder.length; i++) {
+      let moduleName = modulesHolder[i];
+      let mod = require("./modules/" + moduleName);
+      for (let f in mod) {
+        methods[f] = async function(a, b, c, d, e) { return await mod[f](jar, a, b, c, d, e); };
+      }
+    }
+    return methods;
+  });
+}
+*/
+var login = require("./includes/loginToken.js");
+
+module.exports = async function(type, token) {
+  return await login(type, token).then(function(jar) {
     var methods = {};
 
     var modulesHolder = fs.readdirSync("./roblox/modules").filter(function(file) {
